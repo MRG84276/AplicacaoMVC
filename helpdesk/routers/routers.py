83aa31cd2ad84a9a2fd23143e4f181ab0a4732c9
MisterG001 <gustavoyvomartinsa@gmail.com>
2026-08-13 11:42:05 -0300

    Update routers.py

--- helpdesk/routers/routers.py
+++ helpdesk/routers/routers.py
@@ -1,8 +1,10 @@
 from flask import Blueprint
 
-from controllers.controllers import UsuarioController
+from controllers.usuario_controller import UsuarioController
+from controllers.chamado_controller import ChamadoController
 
 usuario_bp = Blueprint("usuario_bp", __name__)
+chamado_bp = Blueprint("chamado_bp", __name__)
 
 usuario_bp.add_url_rule('/', view_func=UsuarioController.index, methods=['GET'])
 usuario_bp.add_url_rule('/usuarios', view_func=UsuarioController.listar, methods=['GET'])
@@ -11,3 +13,12 @@ usuario_bp.add_url_rule('/usuarios/<int:id>', view_func=UsuarioController.atuali
 usuario_bp.add_url_rule('/usuarios/<int:id>', view_func=UsuarioController.excluir, methods=['DELETE'])
 usuario_bp.add_url_rule('/usuarios/<int:id>/ativar', view_func=UsuarioController.ativar, methods=['POST'])
 usuario_bp.add_url_rule('/usuarios/<int:id>/desativar', view_func=UsuarioController.desativar, methods=['POST'])
+
+
+chamado_bp.add_url_rule('/chamados', view_func=ChamadoController.listar, methods=['GET'])
+chamado_bp.add_url_rule('/chamados/<int:id>', view_func=ChamadoController.buscar_por_id, methods=['GET'])
+chamado_bp.add_url_rule('/chamados', view_func=ChamadoController.cadastrar, methods=['POST'])
+chamado_bp.add_url_rule('/chamados/<int:id>', view_func=ChamadoController.atualizar, methods=['PUT'])
+chamado_bp.add_url_rule('/chamados/<int:id>', view_func=ChamadoController.excluir, methods=['DELETE'])
+chamado_bp.add_url_rule('/chamados/<int:id>/tecnico', view_func=ChamadoController.atribuir_tecnico, methods=['POST', 'PATCH'])
+chamado_bp.add_url_rule('/chamados/<int:id>/fechar', view_func=ChamadoController.fechar, methods=['POST'])

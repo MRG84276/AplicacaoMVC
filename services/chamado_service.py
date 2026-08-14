37263ba5f31baee71b0ed98f0b7fbd1649fd3c05
MisterG001 <gustavoyvomartinsa@gmail.com>
2026-08-14 05:14:55 -0300

    Update chamado_service.py

--- services/chamado_service.py
+++ services/chamado_service.py
@@ -1,4 +1,6 @@
 from repositories.chamado_repository import ChamadoRepository
+from repositories.usuario_repository import UsuarioRepository
+
 
 class ChamadoService:
 
@@ -8,8 +10,8 @@ class ChamadoService:
         return [chamado.to_dict() for chamado in chamados]
 
     @staticmethod
-    def buscar_por_id(chamado_id: int):
-        chamado = ChamadoRepository.buscar_por_id(chamado_id)
+    def buscar_por_id(id: int):
+        chamado = ChamadoRepository.buscar_por_id(id)
         return chamado.to_dict() if chamado else None
 
     @staticmethod
@@ -23,20 +25,66 @@ class ChamadoService:
         return novo_chamado.to_dict() if novo_chamado else None
 
     @staticmethod
-    def atualiza_chamado(chamado_id: int, **kwargs):
-        chamado_atualizado = ChamadoRepository.atualizar_chamado(chamado_id, kwargs)
+    def atualiza_chamado(id: int, **kwargs):
+        chamado_atualizado = ChamadoRepository.atualizar_chamado(id, kwargs)
         return chamado_atualizado.to_dict() if chamado_atualizado else None
 
     @staticmethod
-    def exclui_chamado(chamado_id: int):
-        return ChamadoRepository.excluir_chamado(chamado_id)
+    def exclui_chamado(id: int):
+        return ChamadoRepository.excluir_chamado(id)
 
     @staticmethod
-    def atribuir_tecnico(chamado_id: int, tecnico: str):
-        chamado = ChamadoRepository.atribuir_tecnico(chamado_id, tecnico)
+    def atribuir_tecnico(id: int, tecnico: str):
+        chamado = ChamadoRepository.atribuir_tecnico(id, tecnico)
         return chamado.to_dict() if chamado else None
 
     @staticmethod
-    def fechar_chamado(chamado_id: int):
-        chamado = ChamadoRepository.fechar_chamado(chamado_id)
+    def inicia_chamado(id: int):
+       
+        chamado = ChamadoRepository.iniciar_chamado(id)
         return chamado.to_dict() if chamado else None
+
+    @staticmethod
+    def encerra_chamado(id: int):
+        chamado = ChamadoRepository.fechar_chamado(id)
+        return chamado.to_dict() if chamado else None
+
+    @staticmethod
+    def fechar_chamado(id: int):
+        chamado = ChamadoRepository.fechar_chamado(id)
+        return chamado.to_dict() if chamado else None
+
+    @staticmethod
+    def consulta_chamados_abertos():
+        chamados = ChamadoRepository.consulta_tudo()
+        return [c.to_dict() for c in chamados if c.status == "Aberto"]
+
+    @staticmethod
+    def consulta_prioridade_alta():
+        chamados = ChamadoRepository.consulta_tudo()
+        return [c.to_dict() for c in chamados if c.prioridade == "Alta"]
+
+    @staticmethod
+    def obter_estatisticas():
+        todos_usuarios = UsuarioRepository.consulta_tudo()
+        todos_chamados = ChamadoRepository.consulta_tudo()
+
+        abertos = 0
+        em_atendimento = 0
+        encerrados = 0
+
+        for c in todos_chamados:
+            if c.status == "Aberto":
+                abertos += 1
+            elif c.status == "Em atendimento":
+                em_atendimento += 1
+            elif c.status == "Encerrado":
+                encerrados += 1
+
+        return {
+            "usuarios": len(todos_usuarios),
+            "chamados": len(todos_chamados),
+            "abertos": abertos,
+            "em_atendimento": em_atendimento,
+            "encerrados": encerrados
+        }

c7970f93257dd093a311d40df4454448d9f2c0ff
MisterG001 <gustavoyvomartinsa@gmail.com>
2026-08-23 22:10:18 -0300

    Update usuario_controller.py

--- helpdesk/controllers/usuario_controller.py
+++ helpdesk/controllers/usuario_controller.py
@@ -4,9 +4,10 @@ from helpdesk.models.usuario import Usuario
 
 usuario_bp = Blueprint('usuario_bp', __name__)
 
+
 @usuario_bp.route('/', methods=['GET'])
 def listar_usuarios():
-    usuarios = Usuario.get_all()
+    usuarios = UsuarioRepository.consulta_tudo()
     return jsonify([u.to_dict() for u in usuarios]), 200
 
 @usuario_bp.route('/', methods=['POST'])
@@ -14,31 +15,23 @@ def criar_usuario():
     dados = request.get_json()
     if not dados.get('nome') or not dados.get('email'):
         return jsonify({'erro': 'Nome e e-mail são obrigatórios.'}), 400
+        
     usuario = UsuarioRepository.cadastrar_usuario(dados)
-
     if not usuario:
-        return jsonify({'erro': erro}), 400
+        return jsonify({'erro': 'Não foi possível cadastrar o usuário. Verifique se o e-mail já existe'}), 400
 
     return jsonify(usuario.to_dict()), 201
 
 
 @usuario_bp.route('/<int:id>', methods=['GET'])
 def obter_usuario(id):
-    usuario = Usuario.get_or_404(id)
+    usuario = UsuarioRepository.buscar_por_id(id)
     return jsonify(usuario.to_dict()), 200
 
 @usuario_bp.route('/<int:id>', methods=['PUT'])
 def atualizar_usuario(id):
-    usuario = Usuario.query.get_or_404(id)
-    dados = request.get_json()
-
-    usuario.nome = dados.get('nome', usuario.nome)
-    usuario.email = dados.get('email', usuario.email)
-    usuario.setor = dados.get('setor', usuario.setor)
-    if 'ativo' in dados:
-        usuario.ativo = dados['ativo']
-
-    usuario = UsuarioRepository.atualizar_usuario(id,dados)
+    dados = request.get_json() 
+    usuario = UsuarioRepository.atualizar_usuario(id, dados)
 
     if not usuario:
         return jsonify({'erro': f'Usuário com ID {id} não encontrado para atualização.'}), 404
@@ -71,4 +64,5 @@ def desativar_usuario(id):
         return jsonify({'erro': f'Usuário com ID {id} não encontrado.'}), 404
 
     return jsonify(usuario.to_dict()), 200
+
     
